b75e9a98b4b72c9d10e4a27e2a6d94c8f5ad9968
Ricardo Sanchez Alba <risanchez@uclv.cu>
2020-09-16 18:17:15 -0400

    sanitize number and name

--- api/modules/v1/controllers/CallController.php
+++ api/modules/v1/controllers/CallController.php
@@ -14,12 +14,19 @@ class CallController extends Controller
 
         $client = new Client($sid, $token);
 
+        $twilio_number = '+18174096444';
+        $current_user_phone = '+525511393375';
+        $current_user_name = 'Roddy';
+        $provider_phone_number = '+52 55 3453 0087';
+        $provider_name = 'Masiel';
+
+        $twiml_url = 'http://167.99.171.118:8888/twilio.php?name='.$current_user_name.'&providerPhone='.urlencode($provider_phone_number);
 // Read TwiML at this URL when a call connects (hold music)
         $call = $client->calls->create(
-            '+525511393375', // Call this number
-            '+18174096444', // From a valid Twilio number
+            $current_user_phone, // Call this number
+            $twilio_number, // From a valid Twilio number
             [
-                'url' => 'http://167.99.171.118:8888/twilio.php'
+                'url' => $twiml_url
             ]
         );
     }
--- api/web/twilio.php
+++ api/web/twilio.php
@@ -1,9 +1,21 @@
 <?php
 require_once "../vendor/twilio/sdk/src/Twilio/autoload.php";
 
+$name = 'Caller';
+$provider_phone = '+1 111111111';
+
+
+if(isset($_GET['name'])) {
+    $name = preg_replace('/[^-a-zA-Z]/', '', $_GET['name']);
+}
+
+if(isset($_GET['providerPhone'])) {
+    $provider_phone =  preg_replace('/[^0-9+-]/', '', $_GET['providerPhone']);
+}
+
 $response = new Twilio\TwiML\VoiceResponse();
-$response->say('Hello, please wait for the provider');
+$response->say('Hello '.$name.', please wait for the provider');
 //$response->play('https://api.twilio.com/cowbell.mp3');
-$response->dial('+52 55 3453 0087');
+$response->dial($provider_phone);
 $response->say('Goodbye');
 print $response;
