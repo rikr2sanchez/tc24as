18ed2b2f0be0bded373bf56795ebc52688cb2946
Ricardo Sanchez Alba <risanchez@uclv.cu>
2020-09-16 14:07:30 -0400

    testing twilio

--- api/web/twilio.php
+++ api/web/twilio.php
@@ -2,8 +2,8 @@
 require_once "../vendor/twilio/sdk/src/Twilio/autoload.php";
 
 $response = new Twilio\TwiML\VoiceResponse();
-$response->say('Hello');
-$response->play('https://api.twilio.com/cowbell.mp3', ['loop' => 5]);
+$response->say('Hello, please wait for the provider');
+//$response->play('https://api.twilio.com/cowbell.mp3');
 $response->dial('+52 55 3453 0087');
 $response->say('Goodbye');
 print $response;
