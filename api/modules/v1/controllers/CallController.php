9bde8a0bda4055dc786f9153be48f891eb673546
Ricardo Sanchez Alba <rikr2.sanchezalba@gmail.com>
2020-09-18 13:35:19 -0400

    added generic api entrypoint for twilio call

--- api/modules/v1/controllers/CallController.php
+++ api/modules/v1/controllers/CallController.php
@@ -2,6 +2,7 @@
 
 namespace app\modules\v1\controllers;
 
+use app\models\Setting;
 use yii\web\Controller;
 use Twilio\Rest\Client;
 use Twilio\TwiML\VoiceResponse;
@@ -9,22 +10,30 @@ use Twilio\TwiML\VoiceResponse;
 class CallController extends Controller
 {
     public function actionIndex() {
-        $sid = "AC27b919b20f36e2831a2d349bfbccf3f0"; // Your Account SID from www.twilio.com/console
-        $token = "d4c94170174f4b0ab4203f39e76930e3"; // Your Auth Token from www.twilio.com/console
+
+        $request = \Yii::$app->request;
+
+        $sid_setting = Setting::find()->where(['meta_key' => 'twilio_sid'])->one();
+        $sid = $sid_setting->meta_value;
+
+        $token_setting = Setting::find()->where(['meta_key' => 'twilio_token'])->one();
+        $token = $token_setting->meta_value;
+
+        $twilio_phone_setting = Setting::find()->where(['meta_key' => 'twilio_phone'])->one();
+        $twilio_phone = $twilio_phone_setting->meta_value;
+
+        $admin_name = $request->get('adminName'); //TODO: get from User model data
+        $admin_phone = $request->get('adminPhone'); //TODO: get from User model data
+
+        $provider_phone_number = $request->get('providerPhone');
+
+        $twiml_url = 'http://167.99.171.118/api/twilio?name='.$admin_name.'&providerPhone='.urlencode($provider_phone_number);
 
         $client = new Client($sid, $token);
 
-        $twilio_number = '+18174096444';
-        $current_user_phone = '+525511393375';
-        $current_user_name = 'Roddy';
-        $provider_phone_number = '+52 55 3453 0087';
-        $provider_name = 'Masiel';
-
-        $twiml_url = 'http://167.99.171.118:8888/twilio.php?name='.$current_user_name.'&providerPhone='.urlencode($provider_phone_number);
-// Read TwiML at this URL when a call connects (hold music)
         $call = $client->calls->create(
-            $current_user_phone, // Call this number
-            $twilio_number, // From a valid Twilio number
+            $admin_phone, // Call this number
+            $twilio_phone, // From a valid Twilio number
             [
                 'url' => $twiml_url
             ]
