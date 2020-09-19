7d6ef5221755bfd57932baac4390ef3fab2e7c54
Ricardo Sanchez Alba <rikr2.sanchezalba@gmail.com>
2020-09-19 00:42:34 -0400

    fix extra slash in twiml url

--- api/modules/v1/controllers/CallController.php
+++ api/modules/v1/controllers/CallController.php
@@ -27,7 +27,7 @@ class CallController extends Controller
 
         $provider_phone_number = $request->get('providerPhone');
 
-        $twiml_url = 'https://143.110.146.230//api/twilio?name='.$admin_name.'&providerPhone='.urlencode($provider_phone_number);
+        $twiml_url = 'https://143.110.146.230/api/twilio?name='.$admin_name.'&providerPhone='.urlencode($provider_phone_number);
 
         $client = new Client($sid, $token);
 
